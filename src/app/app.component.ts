30d037516d7f658db0844abbf50c39a566fb4121
Jonathan Rojas <jorosa1718daw2@gmail.com>
2018-01-14 20:13:01 +0100

    node + playlist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http/src/response';
+// import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
+interface UserResponse {
+  nombre: string;
+}
 
 @Component({
-  moduleId:module.id,
+  moduleId: module.id,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app';
+
+ 
 }
+/*
+
+@Component({
+  selector: 'dialog',
+  templateUrl: 'dialog.component.html',
+})
+
+export class DialogOverviewExample {
+  
+    animal: string;
+    name: string;
+  
+    constructor(public dialog: MatDialog) {}
+  
+    openDialog(): void {
+      let dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+        width: '250px',
+        data: { name: this.name, animal: this.animal }
+      });
+  
+      dialogRef.afterClosed().subscribe(result => {
+        console.log('The dialog was closed');
+        this.animal = result;
+      });
+    }
+  
+  }
+  
+  @Component({
+    selector: 'dialog-view',
+    templateUrl: 'dialog-view.component.html',
+  })
+  export class DialogOverviewExampleDialog {
+  
+    constructor(
+      public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+      @Inject(MAT_DIALOG_DATA) public data: any) { }
+  
+    onNoClick(): void {
+      this.dialogRef.close();
+    }*/
+  
+  
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+/*import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
+} from '@angular/material';*/
+//cambiar
+import {HttpModule} from '@angular/http';
+// import {CdkTableModule} from '@angular/cdk/table';
 ///////
 import { RouterModule, Routes } from '@angular/router';
 ///////
@@ -35,12 +72,52 @@ const route: Routes =[
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(route),
     FormsModule
   ],
   exports:[ 
-    RouterModule
+    RouterModule,
+    // CdkTableModule,
+   /* MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,*/
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+   // DemoMaterialModule,
+    // MatNativeDateModule,
+   // ReactiveFormsModule,
    ],
+  //  entryComponents: [DialogOverviewExample, DialogOverviewExampleDialog],
   providers: [],
   bootstrap: [RouteComponent]
 })
